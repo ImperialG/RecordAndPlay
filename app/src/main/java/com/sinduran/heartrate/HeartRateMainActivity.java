9dc23ddd10fc436c6ee4cf0fbb4053e65356afa1
rick <ricardobusy@live.co.uk>
2015-11-03 17:41:33 +0000

    Created async task to simulate heartRate being calculated

--- app/src/main/java/com/sinduran/heartrate/HeartRateMainActivity.java
+++ app/src/main/java/com/sinduran/heartrate/HeartRateMainActivity.java
@@ -2,9 +2,10 @@ package com.sinduran.heartrate;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.sinduran.androidrecord.HistoryActivity;
 
@@ -13,11 +14,13 @@ import com.sinduran.androidrecord.HistoryActivity;
  */
 public class HeartRateMainActivity extends Activity {
     private static final String DEBUG = "DEBUG";
+    private TextView heartRateTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_alt);
+        heartRateTV = (TextView) findViewById(R.id.heartRateTV);
     }
 
     public void goToHistory(View view) {
@@ -25,8 +28,37 @@ public class HeartRateMainActivity extends Activity {
         startActivity(intent);
     }
 
-    public void calculateHeartRate(View view){
+    public void calculateHeartRate(View view) {
         //Need async task
-        Log.d(DEBUG, "TODO");
+        new UpdateHRTask().execute();
+    }
+
+    public class UpdateHRTask extends AsyncTask<String, Integer, Long> {
+
+        @Override
+        protected void onPreExecute() {
+            heartRateTV.setText("Calculating..");
+        }
+
+        @Override
+        protected Long doInBackground(String... params) {
+            //Sleep to simulate time delay
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Long.valueOf(1);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            heartRateTV.setText("121 bpm");
+        }
     }
 }
