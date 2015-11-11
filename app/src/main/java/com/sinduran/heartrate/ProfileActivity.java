4553ea08dec84a688ea1a3d0c423d3f4421cdab1
Vishaal Anandaraman <theshaal@gmail.com>
2015-11-11 19:54:47 +0000

    proper refactoring this time

--- app/src/main/java/com/sinduran/heartrate/ProfileActivity.java
+++ app/src/main/java/com/sinduran/heartrate/ProfileActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,8 +13,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-import android.widget.Toolbar;
+
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
